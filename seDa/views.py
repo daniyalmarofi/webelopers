e97f81e778ea36906d0a5913060b7ce586d41b81
sepehr <sepilami@ce.sharif.edu>
2019-11-07 14:48:52 +0330

    navbar icons

--- seDa/views.py
+++ seDa/views.py
@@ -6,22 +6,3 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.shortcuts import render, redirect
 
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
-
-
-@login_required
-def home(request):
-    return render(request, 'base.html')
