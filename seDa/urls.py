42e67409ac63adfa207b89a87ff28c6c977bb961
sepehr <sepilami@ce.sharif.edu>
2019-11-07 14:26:45 +0330

    model and view started

--- seDa/urls.py
+++ seDa/urls.py
@@ -22,3 +22,4 @@ urlpatterns = [
 
     url('',include())
 ]
+
--- seDa/views.py
+++ seDa/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect
+
 
 def signup(request):
     if request.method == 'POST':
@@ -15,3 +20,8 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+
+@login_required
+def home(request):
+    return render(request, 'base.html')
