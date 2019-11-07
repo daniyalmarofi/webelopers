6d8326d22ab27c78066e2a01f134038ac5cabf68
sepehr <sepilami@ce.sharif.edu>
2019-11-07 18:01:54 +0330

    jon

--- webel/views.py
+++ webel/views.py
@@ -29,7 +29,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('/home')
         else:
             print(form.error_messages)
     else:
@@ -37,7 +37,6 @@ def signup(request):
     return render(request, 'b_register.html', {'form': form})
 
 
-def login(request):
 
 def loginReq(request):
     return render(request, 'b_login.html')
